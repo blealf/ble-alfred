417d970763d2046315f559f57f51b4d395334fed
Blessing Alfred <blealf@gmail.com>
2018-06-30 14:35:28 -0400

    front end

---
+++ blealf/src/assets/main.js
@@ -0,0 +1,59 @@
+window.onload = function() {
+  var nav = document.getElementById("nav-wrapper");
+  var btn = document.getElementById("menu-btn");
+
+  btn.addEventListener("click", showMenu);
+  window.addEventListener('resize', respond);
+
+  // Adjust navigation on width change
+  function respond(){
+    if (window.innerWidth > 768) {
+      nav.style.display = "grid";
+      
+    }
+
+    if (window.innerWidth < 768) {
+      nav.style.display = "none";
+    }
+  }
+  
+  
+  // navigation for mobile
+  function showMenu() {
+    // nav.style.transition = "display 1s";
+
+    if (nav.style.display == "none"){
+      
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+    
+  }
+
+
+  // Gallery
+  var smallimages1 = document.getElementsByClassName("big-screen");
+  var smallimages2 = document.getElementsByClassName("big-screen2");
+
+  showBig(smallimages1, "front1");
+  showBig(smallimages2, "front2");
+
+  function showBig(array, imgfront){
+    for (var i = 0; i < array.length; i++) {
+      array[i].addEventListener("click", function () {
+        // console.log(this.src);
+        var previousimg = document.getElementById
+          (imgfront);
+        var temp = previousimg.src;
+        var current = this.src;
+        previousimg.src = current;
+        this.src = temp;
+      });
+    }
+  }
+
+  
+
+
+}
