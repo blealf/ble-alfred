26d198055a94b80513d326c3079297676c5cc880
Blessing Alfred <blealf@gmail.com>
2018-07-09 21:36:07 -0400

    adjusted grammar

--- blealf/src/components/About.js
+++ blealf/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => 
     <div className="about">
         <h2>About Blessing Alfred</h2>
-        <p>To use my skills and personal experience with programminng languages and software development concepts to secure a job in sofware development. To develop high-end softwares and web applications for clients in an organization.</p>
+        <p>To use my skills and personal experience with programming languages and software development concepts to secure a job in sofware development. To develop high-end softwares and web applications for clients in an organization.</p>
     </div>
 
 export default About;
