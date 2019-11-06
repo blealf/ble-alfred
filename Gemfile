583355c7a7ca83151afac8840a331e1c6963a061
BLESSING ALFRED <blealf@gmail.com>
2019-11-05 19:07:14 -0500

    new design

--- Gemfile
+++ Gemfile
@@ -1,12 +1,16 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.1'
+# ruby '2.5.1'
+ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.0'
+# gem 'rails', '~> 5.2.0'
+gem 'rails', '~> 5.2.3'
+
 # Use postgresql as the database for Active Record
-gem 'pg', '>= 0.18', '< 2.0'
+# gem 'pg', '>= 0.18', '< 2.0'
+gem 'pg', '~> 0.18.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- blealf/src/components/Top.js
+++ blealf/src/components/Top.js
@@ -6,16 +6,16 @@ const Top = () =>
             <div className="sets"><img src={require("../assets/images/prog.png")} alt="" /></div>
             <div className="sets">
                 <h2>Blessing Alfred</h2>
-                <h4>Phone: </h4>
-                <p> <a href="tel:5195743928"> 5195743928</a></p>
+                {/*<h4>Phone: </h4>
+                <p> <a href="tel:5195743928"> 5195743928</a></p>*/}
                 <h4 >Email:</h4>
                 <p><a href="mailto:example@email.com">blealfrails@gmail.com</a></p>
                 <h4>Address:</h4>
-                <p> <a href="https://www.google.com/maps/place/1536+Nairn+Ave,+London,+ON+N5V+2P2/@43.0203293,-81.1948518,17z/data=!3m1!4b1!4m5!3m4!1s0x882eed1a14d2a065:0x90295049ef4b5a2c!8m2!3d43.0203254!4d-81.1926578" target="_blank" rel="noopener noreferrer" >1536 Nairn Avenue, London, Ontario. N5V 2P2</a></p>
+                <p> Ontario, Canada</p> 
             </div>
             <div className="social">
                 <div className="social-inner-wrap">
-                    {/* <a href="https://www.instagram.com/blealf/?hl=en" target="_blank" rel="noopener noreferrer">
+                     <a href="https://www.instagram.com/blealf/?hl=en" target="_blank" rel="noopener noreferrer">
                         <i className="f1 fab fa-instagram"></i>
                     </a>
                     <a href="https://plus.google.com/u/0/109786323889831722236" target="_blank" rel="noopener noreferrer">
@@ -29,7 +29,7 @@ const Top = () =>
                     </a>
                     <a href="https://github.com/blealf" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-github"></i>
-                    </a> */}
+                    </a> 
                 </div>
             </div>
         </div>
